1d472e7b41f4d3af5069a5dc88437e0d5144ff10
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2023-10-03 10:00:48 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,8 +1,11 @@
 alert('hello,yuki!');
 alert(1+1);
 alert(Math.sqrt(1+1));
-alert(2+3)+
+alert(Math.sqrt(2+3));
+alert(Math.sqrt(1/0));
+alert(Math.sqrt(0/0));
+alert(2+3);
 alert('2'+'3');
-alert(number('2')+number('3');
+alert(Number('2')+Number('3'));
 alert('2'+3);
-alert(number('2')+3);
+alert(Number('2')+3);
