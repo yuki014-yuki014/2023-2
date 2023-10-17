cda477d360b107934a80cb560cedebb5ea4c0271
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2023-10-17 10:25:00 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,6 +1,8 @@
 var who65 = 'world';
+
 function sayhello65(){alert('Hello, '+ who65 + '!');}
 function taro65(){who65 = '太郎';}
 function hanako65(){who65 = '花子';}                  
+
 function taro66(){alert('太郎' +'が来た');}
 function hanako66(){alert('花子' +'が来た');}
