a1ba4e75cb566d3b27241eac454c8c5f57c0d93f
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2023-10-24 10:04:55 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -1,3 +1,3 @@
 function syahello68(){alert('Hello, ' + document.getElrmentById('who68').innerHTML + '!');}
-function taro68(){document.getElrmentById('who68').innerHTML = '太郎’；｝
-function taro68(){document.getElrmentById('who68').innerHTML = '花子’；｝  
+function taro68(){document.getElrmentById('who68').innerHTML = '太郎'；｝
+function taro68(){document.getElrmentById('who68').innerHTML = '花子'；｝  
