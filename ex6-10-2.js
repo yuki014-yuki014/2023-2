daa95c1a2842186e7c4aa4b4fd8985835e1d8332
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2023-11-14 10:32:18 +0900

    Update ex6-10-2.js

--- ex6-10-2.js
+++ ex6-10-2.js
@@ -1,3 +1,3 @@
 var img;
-function imgchange3(){document.getElementById('logo').src='';}
+function imgchange3(){document.getElementById('logo').src='./image/1-61.jpg';}
 function imgchange4(){document.getElementById('logo').src='';}
