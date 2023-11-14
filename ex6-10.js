914e06131ff70b1614e24b2bcc4232838590c4a7
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2023-11-14 10:03:40 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,1 +1,2 @@
 function imagchange1(){document.getElementById('logo').src='./image/dog_shibainu_black.png';}
+function imagchange1(){document.getElementById('logo').src='./image/dog_shibainu_black.png';}
