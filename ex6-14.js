7f5d35b2dd9c6467490f50785388b92388adf8a4
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2023-11-24 19:31:13 +0900

    Create ex6-14.js

---
+++ ex6-14.js
@@ -0,0 +1,3 @@
+var img;
+function imgchange5(){document.getElementById('logo3').src='./image/1-61.jpg';}
+function imgchange6(){document.getElementById('logo3').src='./image/png-transparent-josai-university-hd-logo.png';}
