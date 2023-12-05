18de021f3ddcfe2b53b4350c4fd6c5e347f84e8d
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2023-12-05 10:11:43 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,4 +1,4 @@
 function add(){
   var x = document.getElementById('input3').value;
-  var y = document.getElementById('input4').value;/100
+  var y = document.getElementById('input4').value;
   alert((x)/(y**2));}
