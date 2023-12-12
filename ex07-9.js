d954bf12be9be116d5633ab679bf86be8418d998
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2023-12-12 10:16:58 +0900

    Update ex07-9.js

--- ex07-9.js
+++ ex07-9.js
@@ -1,7 +1,7 @@
 function nazo79() {
   if (document.getElementById('answer79').value == '夢') {
     alert('あたり!');
-  } else if (document.getElementById('answer79').value == 'ゆめ') {
+  } else if (document.getElementById('answer79').value = 'ゆめ') {
     alert('あたり!');
   } else {    
     alert('はずれ!');
