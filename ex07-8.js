1720855bc895e329dc61868530d94a082293912a
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2023-12-12 10:16:33 +0900

    Update ex07-8.js

--- ex07-8.js
+++ ex07-8.js
@@ -1,5 +1,5 @@
 function nazo78() {
-  if (document.getElementById('answer78').value == '夢') {
+  if (document.getElementById('answer78').value = '夢') {
     alert('あたり!');
   } else {
     alert('はずれ!');
