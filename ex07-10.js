ac84e958f315ba919cc6cb32954c7c5c360c9987
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2024-01-04 00:50:41 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,1 +1,14 @@
+function BMI2() {
+    var x2 = Number(document.getElementById('height2').value) / 100; // メートルに
+    var y2 = Number(document.getElementById('weight2').value);
+    var bmi2 = y2 / (x2 * x2); // BMIの計算
 
+    var siwake;
+  if(bmi2<18.5){
+       siwake='低体重';}
+  else if(bmi2<25){
+       siwake='普通';}
+  else {siwake='肥満';}  
+    
+    alert('あなたのBMIは'+bmi2+'で、評価は['+siwake+']です。');
+}
