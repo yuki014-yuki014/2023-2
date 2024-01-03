feb274c6dcc7b93313f12f01c432fb19ff30b825
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2024-01-04 01:57:40 +0900

    Update ex07-9.js

--- ex07-9.js
+++ ex07-9.js
@@ -1,9 +1,8 @@
-function nazo79() {
-  if (document.getElementById('answer79').value == '夢') {
-    alert('あたり!');
-  } else if (document.getElementById('answer79').value = 'ゆめ') {
-    alert('あたり!');
-  } else {    
-    alert('はずれ!');
+function nazo79(){
+  if(document.getElementById('answer79').value=='夢'){
+    alert('あたり！');}
+  else if(document.getElementById('answer79').value=='ゆめ'){
+    alert('あたり！');}
+  else{
+    alert('はずれ！');}
   }
-}
