f84130fb41b739cf42b1f470eeea6244cc614911
yuki014-yuki014 <131091980+yuki014-yuki014@users.noreply.github.com>
2024-01-04 02:03:48 +0900

    Update ex07-8.js

--- ex07-8.js
+++ ex07-8.js
@@ -1,6 +1,7 @@
 function nazo78(){
-  if(document.getElementById('answer78').value=='夢'){
-    alert('あたり！');}
-  else{
-    alert('はずれ！');}
+  if(document.getElementById('answer78').value == '夢'){
+    alert('あたり！');
+  }else if(document.getElementById('answer78').value = '夢'){
+    alert('はずれ！');
   }
+}
